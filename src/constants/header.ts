4f52a9be07ab41f5488a8c8b65136358d02d456e
HuyenPT <huyenpthe140045@fpt.edu.vn>
2022-11-26 18:13:26 +0700

    update field leave benefit

--- src/constants/header.ts
+++ src/constants/header.ts
@@ -250,8 +250,8 @@ export const DepartmentHeader = [
   },
   {
     title: 'Total members',
-    key: 'total',
-    dataIndex: 'total',
+    key: 'totalEmployee',
+    dataIndex: 'totalEmployee',
   },
 ];
 
@@ -280,11 +280,39 @@ export const LeaveBudgetListHeader = [
     dataIndex: 'used',
   },
   {
-    title: 'Remaining',
-    key: 'remain',
-    dataIndex: 'remain',
+    title: 'Year Remaining',
+    key: 'remainOfYear',
+    dataIndex: 'remainOfYear',
   },
 ];
+export const OTBudgetListHeader = [
+  {
+    title: 'Employee Name',
+    key: 'fullName',
+    dataIndex: 'fullName',
+  },
+  {
+    title: 'Budget',
+    key: 'budget',
+    dataIndex: 'budget',
+  },
+  {
+    title: 'Used',
+    key: 'used',
+    dataIndex: 'used',
+  },
+  {
+    title: 'Month Remaining',
+    key: 'remainOfMonth',
+    dataIndex: 'remainOfMonth',
+  },
+  {
+    title: 'Year Remaining',
+    key: 'remainOfYear',
+    dataIndex: 'remainOfYear',
+  },
+];
+
 export const MyLeaveBudgetListHeader = [
   {
     title: 'Leave Type',
@@ -292,18 +320,40 @@ export const MyLeaveBudgetListHeader = [
     dataIndex: 'requestTypeName',
   },
   {
-    title: 'Leave Budget',
+    title: 'Budget',
     key: 'budget',
     dataIndex: 'budget',
   },
   {
-    title: 'Number Of Day Off',
+    title: 'Used',
     key: 'used',
     dataIndex: 'used',
   },
   {
-    title: 'Remain Day Off',
-    key: 'remain',
-    dataIndex: 'remain',
+    title: 'Year Remaining',
+    key: 'remainOfYear',
+    dataIndex: 'remainOfYear',
+  },
+];
+export const MyOTBudgetListHeader = [
+  {
+    title: 'Budget',
+    key: 'budget',
+    dataIndex: 'budget',
+  },
+  {
+    title: 'Used',
+    key: 'used',
+    dataIndex: 'used',
+  },
+  {
+    title: 'Month Remaining',
+    key: 'remainOfMonth',
+    dataIndex: 'remainOfMonth',
+  },
+  {
+    title: 'Year Remaining',
+    key: 'remainOfYear',
+    dataIndex: 'remainOfYear',
   },
 ];
